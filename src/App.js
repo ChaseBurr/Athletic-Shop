7d08f66112cfa3f3f43e1702362c10556da01ff7
Chase <chase@chaseburr.com>
2021-01-20 15:58:47 -0700

    updated routes and added item page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./App.css";
 
 import Home from "./pages/Home";
 import Store from "./pages/Store";
+import Cart from "./pages/Cart";
+import ItemPage from "./pages/ItemCard";
 import Nav from "./components/Navigation/Nav";
 
-function App() {
+export default () => {
    return (
       <div className="App">
          <Router>
@@ -14,10 +16,10 @@ function App() {
             <Switch>
                <Route exact path="/" component={Home} />
                <Route exact path="/store" component={Store} />
+               <Route exact path="/cart" component={Cart} />
+               <Route exact path="/item/:id" component={ItemPage} />
             </Switch>
          </Router>
       </div>
    );
-}
-
-export default App;
+};
