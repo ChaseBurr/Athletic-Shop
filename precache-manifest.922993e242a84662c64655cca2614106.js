07999d18e9a126c0e395c133b1f3f01b1bfc72cd
Chase <chase@chaseburr.com>
2021-01-20 14:57:48 -0700

    Updates

---
+++ precache-manifest.922993e242a84662c64655cca2614106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5150fab8d50c59b6c313dd369c566e78",
+    "url": "/Athletic-Shop/index.html"
+  },
+  {
+    "revision": "4c49fefc7a47a13032c1",
+    "url": "/Athletic-Shop/static/css/main.b7f5a5f0.chunk.css"
+  },
+  {
+    "revision": "8c325213d431f14883e1",
+    "url": "/Athletic-Shop/static/js/2.fa88b4af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Athletic-Shop/static/js/2.fa88b4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c49fefc7a47a13032c1",
+    "url": "/Athletic-Shop/static/js/main.95d44af4.chunk.js"
+  },
+  {
+    "revision": "608d8e323a081de77d16",
+    "url": "/Athletic-Shop/static/js/runtime-main.0059fe91.js"
+  }
+]);
