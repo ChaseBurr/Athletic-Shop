d987c4167c541c58ec56e2b8a53591dda0efbf03
Chase <chase@chaseburr.com>
2021-01-28 09:57:44 -0700

    added component exports

---
+++ src/components/exports.js
@@ -0,0 +1,4 @@
+export { default as Button } from "./Button/Button";
+export { default as Cards } from "./Cards/Card";
+export { default as Hero } from "./Hero/Hero";
+export { default as Store } from "./Store/Store";
