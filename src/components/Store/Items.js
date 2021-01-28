63f735bdf502f8b183a099cfe63ff1ab4610b3bb
Chase <chase@chaseburr.com>
2021-01-28 09:56:07 -0700

    added items to display

---
+++ src/components/Store/Items.js
@@ -0,0 +1,46 @@
+export const items = [
+   {
+      name: "Bai",
+      description: "",
+      price: "10",
+      price_with_symbol: "$10",
+      imgSrc: "",
+   },
+];
+
+export const promotional = [
+   {
+      name: "Bai Drink",
+      description: "",
+      price: "10",
+      price_with_symbol: "$10",
+      imgSrc: "",
+   },
+];
+
+export const newProducts = [
+   {
+      name: "Vitamin-Mineral",
+      description: "Antioxidant",
+      price: "50",
+      price_with_symbol: "$50",
+      imgSrc:
+         "https://www.maisonbeljanski.com/media/catalog/product/cache/56d045cfb77cba736eab5142e1535b4e/v/i/vitaminmineralantioxidant_copy_1024x1024_.jpg",
+   },
+   {
+      name: "Burn Evolved",
+      description: "Multivitamin For Men",
+      price: "30",
+      price_with_symbol: "$30",
+      imgSrc:
+         "https://d1rolxk7wi0t82.cloudfront.net/media/offers/312/burn-evolved-front/burn-evolved-front.png",
+   },
+   {
+      name: "Triumph",
+      description: "Multivitamin For Men",
+      price: "10",
+      price_with_symbol: "$10",
+      imgSrc:
+         "https://legionathletics.com/wp-content/uploads/2015/08/TriumphM-New-bottle.png",
+   },
+];
