61c4b008f039068b10c4fbcc02b1525490541136
Chase <chase@chaseburr.com>
2021-01-28 09:56:30 -0700

    updated stor maping

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class Store extends Component {
-  static propTypes = {
-    prop: PropTypes,
-  };
+   static propTypes = {
+      prop: PropTypes,
+   };
 
-  render() {
-    return (
-      <div className="store">
-        <div className="store-item">asdf</div>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="store">
+            <div className="store-item">Store</div>
+         </div>
+      );
+   }
 }
